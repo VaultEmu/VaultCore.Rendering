1889a1121cbf9fb05c1944b6b1610df679fb7989
Chris Butler <chrisbutler@polygonhearts.com>
2022-12-29 19:37:10 +0000

    Updated VaultCore Package and fixed features

--- Source/Features/ISoftwareRendering.cs
+++ Source/Features/ISoftwareRendering.cs
@@ -5,14 +5,11 @@ namespace VaultCore.Rendering;
 /// <summary>
 /// Feature for allowing your core to perform software rendering.
 /// </summary>
-public interface ISoftwareRendering : IVaultCoreFeature<ISoftwareRendering.FeatureApi>
+public interface ISoftwareRendering : IVaultCoreFeature
 {
-    public interface FeatureApi : IVaultCoreFeatureApi
-    {
-        /// <summary>
-        /// Called by the core to let the frontend know that there is pixeldata to display
-        /// </summary>
-        /// <param name="pixelData">The pixel data to display</param>
-        void OnFrameReadyToDisplay(PixelData pixelData);
-    }
+    /// <summary>
+    /// Called by the core to let the frontend know that there is pixeldata to display
+    /// </summary>
+    /// <param name="pixelData">The pixel data to display</param>
+    void OnFrameReadyToDisplay(PixelData pixelData);
 }
