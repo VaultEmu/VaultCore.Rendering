3cef6a4344319cadd2c943d1877d77018775f6a0
Chris Butler <chrisbutler@polygonhearts.com>
2023-04-17 20:39:23 +0100

    Added RectFloat Struct

---
+++ Source/Graphics/Math/RectFloat.cs
@@ -0,0 +1,18 @@
+﻿namespace VaultCore.Rendering;
+
+//Represents a rectangle defined by its width, height and position as floats
+public struct RectFloat
+{
+    public float X;
+    public float Y;
+    public float Width;
+    public float Height;
+
+    public RectFloat(float x, float y, float width, float height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+}
