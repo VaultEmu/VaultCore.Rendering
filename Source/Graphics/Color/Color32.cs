e5a9a148e4b0a1a51b4592d480fcaeace0ef47ca
Chris Butler <chrisbutler@polygonhearts.com>
2023-05-07 23:13:26 +0100

    Documented Color structs + added standard colors as static properties

--- Source/Graphics/Color/Color32.cs
+++ Source/Graphics/Color/Color32.cs
@@ -6,19 +6,81 @@ namespace VaultCore.Rendering;
 [StructLayout(LayoutKind.Explicit)]
 public struct Color32
 {
+    /// <summary>
+    /// White (255, 255, 255, 255)
+    /// </summary>
+    public static Color32 White => new Color32(255,255,255);
+    
+    /// <summary>
+    /// Black (0, 0, 0, 255)
+    /// </summary>
+    public static Color32 Black => new Color32(0,0,0);
+    
+    /// <summary>
+    /// Red (255, 0, 0, 255)
+    /// </summary>
+    public static Color32 Red => new Color32(255,0,0);
+    
+    /// <summary>
+    /// Green (0, 255, 0, 255)
+    /// </summary>
+    public static Color32 Green => new Color32(0,255,0);
+    
+    /// <summary>
+    /// Blue (0, 0, 255, 255)
+    /// </summary>
+    public static Color32 Blue => new Color32(0,0,255);
+    
+    /// <summary>
+    /// Cyan (0, 255, 255, 255)
+    /// </summary>
+    public static Color32 Cyan => new Color32(0,255,255);
+    
+    /// <summary>
+    /// Yellow (255, 255, 0, 255)
+    /// </summary>
+    public static Color32 Yellow => new Color32(255,255,0);
+    
+    /// <summary>
+    /// Magenta (255, 0, 255, 255)
+    /// </summary>
+    public static Color32 Magenta => new Color32(255,0,255);
+    
+    /// <summary>
+    /// Clear (0, 0, 0, 0)
+    /// </summary>
+    public static Color32 Clear => new Color32(0,0,0,0);
+    
+    /// <summary>
+    /// Red color component
+    /// </summary>
     [FieldOffset(0)]
     public byte R;
     
+    /// <summary>
+    /// Green Color Component
+    /// </summary>
     [FieldOffset(1)]
     public byte G;
     
+    /// <summary>
+    /// Blue Color Component
+    /// </summary>
     [FieldOffset(2)]
     public byte B;
     
+    /// <summary>
+    /// Alpha Color Component
+    /// </summary>
     [FieldOffset(3)]
     public byte A;
     
-    //Creates a Color32 from rgb byte values
+    /// <summary>
+    /// Creates a Color32 from rgba byte values
+    /// </summary>
+    /// <param name="r">r component (between 0 - 255)</param>
+    /// <param name="g">g component (between 0 - 255)</param>
+    /// <param name="b">b component (between 0 - 255)</param>
     public Color32(byte r, byte g, byte b)
     {
         R = r;
@@ -27,7 +89,13 @@ public struct Color32
         A = 255;
     }
     
-    //Creates a Color32 from rgba byte values
+    /// <summary>
+    /// Creates a Color32 from rgba byte values
+    /// </summary>
+    /// <param name="r">r component (between 0 - 255)</param>
+    /// <param name="g">g component (between 0 - 255)</param>
+    /// <param name="b">b component (between 0 - 255)</param>
+    /// <param name="a">a component (between 0 - 255)</param>
     public Color32(byte r, byte g, byte b, byte a)
     {
         R = r;
@@ -36,8 +104,13 @@ public struct Color32
         A = a;
     }
     
-    //Creates a Color32 from rgb float values
-    //Input components will be clamped between 0-1
+    /// <summary>
+    /// Creates a Color32 from rgba float values
+    /// Input components will be clamped between 0-1
+    /// </summary>
+    /// <param name="r">r component (between 0 - 1)</param>
+    /// <param name="g">g component (between 0 - 1)</param>
+    /// <param name="b">b component (between 0 - 1)</param>
     public Color32(float r, float g, float b)
     {
         R = (byte)(Math.Round(Math.Clamp(r, 0.0f, 1.0f) * 255.0f));
@@ -46,8 +119,14 @@ public struct Color32
         A = 255;
     }
     
-    //Creates a Color32 from rgba float values
-    //Input components will be clamped between 0-1
+    /// <summary>
+    /// Creates a Color32 from rgba float values
+    /// Input components will be clamped between 0-1
+    /// </summary>
+    /// <param name="r">r component (between 0 - 1)</param>
+    /// <param name="g">g component (between 0 - 1)</param>
+    /// <param name="b">b component (between 0 - 1)</param>
+    /// <param name="a">a component (between 0 - 1)</param>
     public Color32(float r, float g, float b, float a)
     {
         R = (byte)(Math.Round(Math.Clamp(r, 0.0f, 1.0f) * 255.0f));
--- Source/Graphics/Color/ColorFloat.cs
+++ Source/Graphics/Color/ColorFloat.cs
@@ -6,18 +6,82 @@ namespace VaultCore.Rendering;
 [StructLayout(LayoutKind.Explicit)]
 public struct ColorFloat
 {
+    /// <summary>
+    /// White (1.0f, 1.0f, 1.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat White => new ColorFloat(1.0f,1.0f,1.0f);
+    
+    /// <summary>
+    /// Black (0.0f, 0.0f, 0.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Black => new ColorFloat(0.0f,0.0f,0.0f);
+    
+    /// <summary>
+    /// Red (1.0f, 0.0f, 0.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Red => new ColorFloat(1.0f,0.0f,0.0f);
+    
+    /// <summary>
+    /// Green (0.0f, 1.0f, 0.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Green => new ColorFloat(0.0f,1.0f,0.0f);
+    
+    /// <summary>
+    /// Blue (0.0f, 0.0f, 1.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Blue => new ColorFloat(0.0f,0.0f,1.0f);
+    
+    /// <summary>
+    /// Cyan (0.0f, 1.0f, 1.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Cyan => new ColorFloat(0.0f,1.0f,1.0f);
+    
+    /// <summary>
+    /// Yellow (1.0f, 1.0f, 0.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Yellow => new ColorFloat(1.0f,1.0f,0.0f);
+    
+    /// <summary>
+    /// Magenta (1.0f, 0.0f, 1.0f, 1.0f)
+    /// </summary>
+    public static ColorFloat Magenta => new ColorFloat(1.0f,0.0f,1.0f);
+    
+    /// <summary>
+    /// Clear (0, 0, 0, 0)
+    /// </summary>
+    public static ColorFloat Clear => new ColorFloat(0,0,0,0);
+    
+    /// <summary>
+    /// Red color component
+    /// </summary>
     [FieldOffset(0)]
     public float R;
     
+    /// <summary>
+    /// Green color component
+    /// </summary>
     [FieldOffset(4)]
     public float G;
     
+    /// <summary>
+    /// Blue color component
+    /// </summary>
     [FieldOffset(8)]
     public float B;
     
+    /// <summary>
+    /// Alpha color component
+    /// </summary>
     [FieldOffset(12)]
     public float A;
     
+    /// <summary>
+    /// Creates a ColorFloat from rgba float values
+    /// Input components will be clamped between 0-1
+    /// </summary>
+    /// <param name="r">r component (between 0 - 1)</param>
+    /// <param name="g">g component (between 0 - 1)</param>
+    /// <param name="b">b component (between 0 - 1)</param>
     public ColorFloat(float r, float g, float b)
     {
         R = r;
@@ -26,6 +90,14 @@ public struct ColorFloat
         A = 1.0f;
     }
     
+    /// <summary>
+    /// Creates a ColorFloat from rgba float values
+    /// Input components will be clamped between 0-1
+    /// </summary>
+    /// <param name="r">r component (between 0 - 1)</param>
+    /// <param name="g">g component (between 0 - 1)</param>
+    /// <param name="b">b component (between 0 - 1)</param>
+    /// <param name="a">a component (between 0 - 1)</param>
     public ColorFloat(float r, float g, float b, float a)
     {
         R = r;
